c7d5e670a6a3d370963a99dc7c22beb8d0c45b1d
woo <choiwu0917@gmail.com>
2022-03-10 16:16:21 +0900

    ...ing

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,9 @@
-import {useParams} from "react-router";
-import { useLocation} from "react-router-dom";
+import {Switch, Route, useParams, useLocation, useRouteMatch} from "react-router-dom";
 import styled from 'styled-components';
-import {useEffect, useState} from 'react';
-import { setPriority } from "os";
+import {useEffect, useState,} from 'react';
+import Chart from './Chart';
+import Price from './Price';
+import {Link} from "react-router-dom";
 
 interface RouteParams{
     coinId: string;
@@ -11,6 +12,107 @@ interface RouteParams{
 interface RouteState{
     name: string;
 }
+
+interface IInfoData{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface IPriceData{
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
+const Tabs=styled.div`
+display:grid;
+grid-template-columns:repeat(2,1fr);
+margin:25px 0px;
+gap:10px;
+`;
+
+const Tab=styled.span<{isActive:boolean}>`
+text-align:center;
+text-transform:uppercase;
+color:white;
+font-size:12px;
+font-weight:400;
+background-color:#0984e3;
+padding:7px 0px;
+border-radius:10px;
+color:${(props)=>props.isActive ? props.theme.accentColor : props.theme.textColor};
+a{
+    display:block;
+}
+`;
+
+const Overview=styled.div`
+display:flex;
+justify-content:space-between;
+background-color:#7ed6df;
+padding:10px 20px;
+border-radius:10px;
+`;
+
+const OverviewItem=styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+span:first-child{
+    font-size:10px;
+    font-weight:400;
+    text-transform:uppercase;
+    margin-bottom:5px;
+}
+`;
+
+const Description=styled.p`
+margin:20px 0px
+`;
 
 const Container=styled.div`
 padding:0px 20px;
@@ -37,10 +139,12 @@ color:${(props)=>props.theme.accentColor};
 
 function Coin(){
     const [loading, setLoading]=useState(true);
-    const [info, setInfo]=useState({});
-    const [priceinfo, setPriceinfo]=useState({});
+    const [info, setInfo]=useState<IInfoData>();
+    const [priceinfo, setPriceinfo]=useState<IPriceData>();
     const {coinId}=useParams<RouteParams>();
     const {state}=useLocation<RouteState>();
+    const PriceMatch=useRouteMatch("/:coinId/price");
+    const ChartMatch=useRouteMatch("/:coinId/chart");
     useEffect(()=>{
         (async ()=>{
             const infoData=await(
@@ -51,14 +155,59 @@ function Coin(){
             ).json();
             setInfo(infoData);
             setPriceinfo(priceData);
+            setLoading(false);
         })();
-    },[]);
-    return (
+    },[coinId]);
+    return(
         <Container>
             <Header>
-                <Title>{state?.name || "Loading...⌛"}</Title>
+                <Title>{state?.name ? state.name: loading? "Loading...⌛":info?.name}</Title>
             </Header>
-            {loading ? <Loader>Loading...⏳</Loader>:null}
+            {loading ? <Loader>Loading...⏳</Loader>:(
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>{info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source</span>
+                            <span>{info?.open_source ? "Yes":"No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Supply:</span>
+                            <span>{priceinfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceinfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Tabs>
+                        <Tab isActive={ChartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={PriceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+                    <Switch>
+                        <Route path={`/:coinId/price`}>
+                            <Price/>
+                        </Route>
+                        <Route path={`/:coinId/chart`}>
+                            <Chart/>
+                        </Route>
+                    </Switch>
+                </>
+            )}
         </Container>
     );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const theme:DefaultTheme={
     bgColor:"#fff200",
     textColor:"black",
-    accentColor:'black',
+    accentColor:'coral',
 };
